71d9ef47fa209812c5b2ffef0add4109c8fa7953
DarkWeird <foxflameless@gmail.com>
2020-09-09 17:06:48 +0300

    feature(reintegrate-gestaltv7):  gestalt-v7 compability (package)

--- src/main/java/org/terasology/mobileBlocks/client/MovingBlockClientSystem.java
+++ src/main/java/org/terasology/mobileBlocks/client/MovingBlockClientSystem.java
@@ -4,26 +4,26 @@ package org.terasology.mobileBlocks.client;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.terasology.utilities.Assets;
-import org.terasology.engine.Time;
-import org.terasology.entitySystem.entity.EntityManager;
-import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.entity.lifecycleEvents.BeforeDeactivateComponent;
-import org.terasology.entitySystem.entity.lifecycleEvents.OnActivatedComponent;
-import org.terasology.entitySystem.event.ReceiveEvent;
-import org.terasology.entitySystem.systems.BaseComponentSystem;
-import org.terasology.entitySystem.systems.RegisterMode;
-import org.terasology.entitySystem.systems.RegisterSystem;
-import org.terasology.entitySystem.systems.UpdateSubscriberSystem;
-import org.terasology.logic.location.LocationComponent;
+import org.terasology.engine.core.Time;
+import org.terasology.engine.entitySystem.entity.EntityManager;
+import org.terasology.engine.entitySystem.entity.EntityRef;
+import org.terasology.engine.entitySystem.entity.lifecycleEvents.BeforeDeactivateComponent;
+import org.terasology.engine.entitySystem.entity.lifecycleEvents.OnActivatedComponent;
+import org.terasology.engine.entitySystem.event.ReceiveEvent;
+import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
+import org.terasology.engine.entitySystem.systems.RegisterMode;
+import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.entitySystem.systems.UpdateSubscriberSystem;
+import org.terasology.engine.logic.location.LocationComponent;
+import org.terasology.engine.registry.In;
+import org.terasology.engine.rendering.assets.mesh.Mesh;
+import org.terasology.engine.rendering.logic.MeshComponent;
+import org.terasology.engine.utilities.Assets;
+import org.terasology.engine.world.block.Block;
 import org.terasology.math.geom.Vector3f;
 import org.terasology.math.geom.Vector3i;
 import org.terasology.mobileBlocks.MovingBlockComponent;
-import org.terasology.registry.In;
-import org.terasology.rendering.assets.mesh.Mesh;
-import org.terasology.rendering.logic.MeshComponent;
 import org.terasology.nui.Color;
-import org.terasology.world.block.Block;
 
 @RegisterSystem(RegisterMode.CLIENT)
 public class MovingBlockClientSystem extends BaseComponentSystem implements UpdateSubscriberSystem {
@@ -37,7 +37,8 @@ public class MovingBlockClientSystem extends BaseComponentSystem implements Upda
     @Override
     public void update(float delta) {
         long gameTimeInMs = time.getGameTimeInMs();
-        for (EntityRef entityRef : entityManager.getEntitiesWith(MovingBlockComponent.class, LocationComponent.class, MeshComponent.class)) {
+        for (EntityRef entityRef : entityManager.getEntitiesWith(MovingBlockComponent.class, LocationComponent.class,
+                MeshComponent.class)) {
             MovingBlockComponent movingBlock = entityRef.getComponent(MovingBlockComponent.class);
 
             Vector3i locationFrom = movingBlock.getLocationFrom();
@@ -66,7 +67,8 @@ public class MovingBlockClientSystem extends BaseComponentSystem implements Upda
     }
 
     @ReceiveEvent
-    public void movingStarted(OnActivatedComponent event, EntityRef entity, MovingBlockComponent movingBlock, LocationComponent locationComponent) {
+    public void movingStarted(OnActivatedComponent event, EntityRef entity, MovingBlockComponent movingBlock,
+                              LocationComponent locationComponent) {
         Block blockToRender = movingBlock.getBlockToRender();
         Mesh mesh = blockToRender.getMeshGenerator().getStandaloneMesh();
 
@@ -81,7 +83,8 @@ public class MovingBlockClientSystem extends BaseComponentSystem implements Upda
     }
 
     @ReceiveEvent
-    public void beforeMovingFinished(BeforeDeactivateComponent event, EntityRef entity, MovingBlockComponent movingBlock, LocationComponent locatinComponent) {
+    public void beforeMovingFinished(BeforeDeactivateComponent event, EntityRef entity,
+                                     MovingBlockComponent movingBlock, LocationComponent locatinComponent) {
         MeshComponent meshComponent = entity.getComponent(MeshComponent.class);
         meshComponent.mesh.dispose();
         entity.removeComponent(MeshComponent.class);
--- src/main/java/org/terasology/mobileBlocks/server/MovingBlockServerSystem.java
+++ src/main/java/org/terasology/mobileBlocks/server/MovingBlockServerSystem.java
@@ -1,41 +1,28 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.mobileBlocks.server;
 
-import org.terasology.engine.Time;
-import org.terasology.entitySystem.entity.EntityManager;
-import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.event.ReceiveEvent;
-import org.terasology.entitySystem.prefab.Prefab;
-import org.terasology.entitySystem.systems.BaseComponentSystem;
-import org.terasology.entitySystem.systems.RegisterSystem;
-import org.terasology.logic.delay.DelayManager;
-import org.terasology.logic.delay.DelayedActionTriggeredEvent;
-import org.terasology.logic.health.event.BeforeDamagedEvent;
-import org.terasology.logic.location.LocationComponent;
-import org.terasology.math.Direction;
+import org.terasology.engine.core.Time;
+import org.terasology.engine.entitySystem.entity.EntityManager;
+import org.terasology.engine.entitySystem.entity.EntityRef;
+import org.terasology.engine.entitySystem.event.ReceiveEvent;
+import org.terasology.engine.entitySystem.prefab.Prefab;
+import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
+import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.logic.delay.DelayManager;
+import org.terasology.engine.logic.delay.DelayedActionTriggeredEvent;
+import org.terasology.engine.logic.location.LocationComponent;
+import org.terasology.engine.math.Direction;
+import org.terasology.engine.registry.In;
+import org.terasology.engine.registry.Share;
+import org.terasology.engine.world.BlockEntityRegistry;
+import org.terasology.engine.world.WorldProvider;
+import org.terasology.engine.world.block.Block;
+import org.terasology.engine.world.block.BlockManager;
+import org.terasology.engine.world.block.entity.placement.PlaceBlocks;
+import org.terasology.health.logic.event.BeforeDamagedEvent;
 import org.terasology.math.geom.Vector3i;
 import org.terasology.mobileBlocks.MovingBlockComponent;
-import org.terasology.registry.In;
-import org.terasology.registry.Share;
-import org.terasology.world.BlockEntityRegistry;
-import org.terasology.world.WorldProvider;
-import org.terasology.world.block.Block;
-import org.terasology.world.block.BlockManager;
-import org.terasology.world.block.entity.placement.PlaceBlocks;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -87,7 +74,8 @@ public class MovingBlockServerSystem extends BaseComponentSystem implements Bloc
         EntityRef movingEntity = entityManager.create(prefab);
         try {
             movingEntity.addComponent(new LocationComponent(location.toVector3f()));
-            movingEntity.addComponent(new MovingBlockComponent(block, location, moveLocation, gameTime, gameTime + moveTime));
+            movingEntity.addComponent(new MovingBlockComponent(block, location, moveLocation, gameTime,
+                    gameTime + moveTime));
             blockEntity.send(new BlockTransitionDuringMoveEvent(true, movingEntity));
 
             Map<Vector3i, Block> blocksToPlace = new HashMap<>();
@@ -158,7 +146,8 @@ public class MovingBlockServerSystem extends BaseComponentSystem implements Bloc
     }
 
     @ReceiveEvent
-    public void preventDamagingOfBlocks(BeforeDamagedEvent event, EntityRef entity, MovingBlockReplacementComponent movingBlockReplacementComponent) {
+    public void preventDamagingOfBlocks(BeforeDamagedEvent event, EntityRef entity,
+                                        MovingBlockReplacementComponent movingBlockReplacementComponent) {
         event.consume();
     }
 }
